801b2a1aaa40ad4cd39a73f4f3becbe018e61e2c
pranavi jamdar <pranavijamdar39@gmail.com>
2026-07-01 17:40:47 +0530

    Add vowel and consonant checker in q1.py

---
+++ q1.py
@@ -0,0 +1,10 @@
+ch = "a"
+if ch == 'a' or ch == 'e'or ch == 'i' or ch == 'o' or ch == 'u' or\
+ch == 'A' or ch == 'E'or ch == 'I'or ch == 'O'or ch == 'U':
+    print("it is vowel:")
+else:
+    print("it is consonant:")
+
+
+
+
