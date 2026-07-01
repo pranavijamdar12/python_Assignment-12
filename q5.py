2fbd8b4b860c612c0921250ce985b9b3dc5767b7
pranavi jamdar <pranavijamdar39@gmail.com>
2026-07-01 19:10:33 +0530

    Add marks classification based on input

---
+++ q5.py
@@ -0,0 +1,10 @@
+marks= int(input("Enter your marks:"))
+if (marks >= 75):
+    print("Distinction:")
+elif(marks >= 60):
+    print("first class:")
+elif(marks >= 50):
+    print("second class:")
+else:
+    print("fail")
+    
